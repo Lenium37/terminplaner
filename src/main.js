79252e54680bcf9f76f8b57179f99114b159884c
johannes <hirth.johannes@gmx.de>
2022-06-24 23:30:35 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import Antd from 'ant-design-vue';
+import App from "./App.vue";
+import router from "./router";
+
+import 'ant-design-vue/dist/antd.css';
+
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(Antd);
+
+app.mount("#app");
